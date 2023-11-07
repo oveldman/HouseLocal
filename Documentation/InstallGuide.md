3c5841e482cebd7e6f565fd2ec40a204384741da
Oscar Veldman <oveldman@gmail.com>
2023-11-07 19:45:00 +0100

    Add the frontend ui

--- Documentation/InstallGuide.md
+++ Documentation/InstallGuide.md
@@ -9,5 +9,6 @@
 ## Edit your host file
 Add the following lines to your host file
 ```bash
-127.0.0.1   my.houseapp.local
+127.0.0.1   api.myhouseapp.local
+127.0.0.1   ui.myhouseapp.local
 ```
--- HouseApp/Deployement/build-and-deploy.sh
+++ HouseApp/Deployement/build-and-deploy.sh
@@ -5,8 +5,6 @@ docker build -f HouseApp.Backend.FormulaOne/Dockerfile -t madworld/houseapp/form
 minikube image load --overwrite madworld/houseapp/formulaone
 echo "FormulaOne image loaded"
 
-minikube kubectl -- apply -f Deployement/Kubernetes
-
 docker build -f HouseApp.Backend.Weather/Dockerfile -t madworld/houseapp/weather .
 minikube image load --overwrite madworld/houseapp/weather 
 echo "Weather image loaded"
@@ -14,5 +12,9 @@ echo "Weather image loaded"
 docker build -f HouseApp.Backend.Light/Dockerfile -t madworld/houseapp/light .
 minikube image load --overwrite madworld/houseapp/light
 echo "Light image loaded"
+
+docker build -f HouseApp.Frontend.UI/Dockerfile -t madworld/houseapp/ui .
+minikube image load --overwrite madworld/houseapp/ui
+echo "UI image loaded"
 
 minikube kubectl -- apply -f Deployement/Kubernetes
