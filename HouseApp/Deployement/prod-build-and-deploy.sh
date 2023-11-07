b34c0884497c32a76eeed2d24b9444fd656a3ca5
Oscar Veldman <oveldman@gmail.com>
2023-11-07 20:54:13 +0100

    Try fix deploy

--- HouseApp/Deployement/prod-build-and-deploy.sh
+++ HouseApp/Deployement/prod-build-and-deploy.sh
@@ -4,22 +4,22 @@ sudo microk8s kubectl delete deployment --all
 
 docker build -f HouseApp.Backend.FormulaOne/Dockerfile -t madworld/houseapp/formulaone .
 docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-formulaone.tar
-sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-formulaone.tar
+sudo microk8s images import < Deployment/Kubernetes/images/madworld-houseapp-formulaone.tar
 echo "FormulaOne image loaded"
 
 docker build -f HouseApp.Backend.Weather/Dockerfile -t madworld/houseapp/weather .
 docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-weather.tar
-sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-weather.tar
+sudo microk8s images import < Deployment/Kubernetes/images/madworld-houseapp-weather.tar
 echo "Weather image loaded"
 
 docker build -f HouseApp.Backend.Light/Dockerfile -t madworld/houseapp/light .
 docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-light.tar
-sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-light.tar
+sudo microk8s images import < Deployment/Kubernetes/images/madworld-houseapp-light.tar
 echo "Light image loaded"
 
 docker build -f HouseApp.Frontend.UI/Dockerfile -t madworld/houseapp/ui .
 docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-ui.tar
-sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-ui.tar
+sudo microk8s images import < Deployment/Kubernetes/images/madworld-houseapp-ui.tar
 echo "UI image loaded"
 
 sudo microk8s kubectl -- apply -f Deployement/Kubernetes
