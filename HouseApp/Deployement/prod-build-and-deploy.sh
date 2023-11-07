611307ab40a8f3a4a9e4b1421f29f16ffeea662f
Oscar Veldman <oveldman@gmail.com>
2023-11-07 20:43:21 +0100

    Try to deploy on server

---
+++ HouseApp/Deployement/prod-build-and-deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo kubectl delete deployment --all
+
+docker build -f HouseApp.Backend.FormulaOne/Dockerfile -t madworld/houseapp/formulaone .
+docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-formulaone.tar
+sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-formulaone.tar
+echo "FormulaOne image loaded"
+
+docker build -f HouseApp.Backend.Weather/Dockerfile -t madworld/houseapp/weather .
+docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-weather.tar
+sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-weather.tar
+echo "Weather image loaded"
+
+docker build -f HouseApp.Backend.Light/Dockerfile -t madworld/houseapp/light .
+docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-light.tar
+sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-light.tar
+echo "Light image loaded"
+
+docker build -f HouseApp.Frontend.UI/Dockerfile -t madworld/houseapp/ui .
+docker save madworld/houseapp/formulaone > Deployment/Kubernetes/images/madworld-houseapp-ui.tar
+sudo microk8s.ctr -n k8s.io image import Deployment/Kubernetes/images/madworld-houseapp-ui.tar
+echo "UI image loaded"
+
+sudo kubectl -- apply -f Deployement/Kubernetes
+
+sudo rm -r Deployment/Kubernetes/images
